19f9b7dce11b7cd1843c6c8b943d9cdc04317bdb
root <root@localhost.localdomain>
2019-10-12 14:59:13 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: install.sh
+# Author: wsl
+# mail: 645003108@qq.com
+# Created Time:Sat 12 Oct 2019 02:50:02 PM CST
+#########################################################################
+#!/bin/bash
+mkdir /usr/local/wsl
+cp wsl /usr/local/wsl
+chmod 0755 -R /usr/local/wsl	
+
